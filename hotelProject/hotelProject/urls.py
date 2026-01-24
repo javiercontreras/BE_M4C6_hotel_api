0c5a98a628542bbc20cf4ed4ad72a87c5cffcc76
FranciscoVega <pancho.vega.90@gmail.com>
2026-01-23 22:28:18 -0300

    url api-token-auth

--- hotelProject/hotelProject/urls.py
+++ hotelProject/hotelProject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from myapp.views import RoomViewSet, Reservartion  
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'rooms', RoomViewSet)
@@ -26,4 +27,5 @@ router.register(r'reservations', Reservartion)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  
 ]
