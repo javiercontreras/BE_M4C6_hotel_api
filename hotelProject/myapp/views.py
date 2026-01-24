de19a3de7de63c955682b7432ff39cdc00422f87
FranciscoVega <pancho.vega.90@gmail.com>
2026-01-23 22:19:27 -0300

    Se agrego app de auth en settings

--- hotelProject/myapp/views.py
+++ hotelProject/myapp/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Room, Reservation
 from .serializers import RoomSerializer, ReservationSerializer
+from rest_framework.permissions import AllowAny #
 
 
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    permission_classes = [AllowAny]
 
 class Reservartion(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
